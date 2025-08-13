c173b5c2f11e07e26ad7c998cb31d19a4733a54d
AutoVideoGen281 <michel123geb@gmail.com>
2025-08-13 11:30:36 +0300

    Add files via upload

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,21 +1,5 @@
 import React from 'react';
 
-export const FlameIcon = ({ className }: { className?: string }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className={className}
-  >
-    <path
-      fillRule="evenodd"
-      d="M12.963 2.286a.75.75 0 00-1.071 1.071l9 9a.75.75 0 001.071-1.071l-9-9zM12 3a9 9 0 100 18 9 9 0 000-18zM3.75 12a8.25 8.25 0 1116.5 0 8.25 8.25 0 01-16.5 0z"
-      clipRule="evenodd"
-    />
-    <path d="M12 7.5a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V8.25A.75.75 0 0112 7.5z" />
-  </svg>
-);
-
 export const TrophyIcon = ({ className }: { className?: string }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="currentColor">
         <path fillRule="evenodd" d="M4.5 3.75a3 3 0 00-3 3v10.5a3 3 0 003 3h15a3 3 0 003-3V6.75a3 3 0 00-3-3h-15zm4.125 3a2.625 2.625 0 115.25 0 2.625 2.625 0 01-5.25 0zM8.25 12a.75.75 0 000 1.5h7.5a.75.75 0 000-1.5h-7.5z" clipRule="evenodd" />
@@ -30,10 +14,6 @@ export const FireIcon = ({ className }: { className?: string }) => (
     fill="currentColor"
     className={className}
   >
-    <path
-      fillRule="evenodd"
-      d="M12.963 2.286a.75.75 0 00-1.071 1.071l9 9a.75.75 0 001.071-1.071l-9-9zM12 3a9 9 0 100 18 9 9 0 000-18zM3.75 12a8.25 8.25 0 1116.5 0 8.25 8.25 0 01-16.5 0z"
-      clipRule="evenodd"
-    />
+    <path d="M12 2.25c.338 0 .67.019.998.055 2.115.151 3.86 1.897 4.012 4.012.036.328.055.66.055.998 0 3.86-1.897 7.213-4.565 9.565a.75.75 0 01-1.06 0C9.147 14.478 7.25 11.123 7.25 7.25c0-.338.019-.67.055-.998.151-2.115 1.897-3.86 4.012-4.012A9.702 9.702 0 0112 2.25z" />
   </svg>
 );
---
+++ sw.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = 'push-up-pro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.tsx',
+  '/App.tsx',
+  '/types.ts',
+  '/constants.ts',
+  '/components/InitialView.tsx',
+  '/components/WorkoutTracker.tsx',
+  '/components/CompletedScreen.tsx',
+  '/components/StatusHeader.tsx',
+  '/components/StartDayView.tsx',
+  '/components/icons.tsx',
+  '/public/icon.svg',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap',
+  'https://esm.sh/react@^19.1.1',
+  'https://esm.sh/react-dom@^19.1.1/client'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          response => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
